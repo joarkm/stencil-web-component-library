38fd5bf847a5f9dc9d618f11e39abe93b21a4720
Joar Krohn Mauseth <jkm99412@gmail.com>
2022-05-08 20:36:05 +0200

    initial commit

---
+++ apps/core-components-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'core-components-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'core-components-app';
+}
